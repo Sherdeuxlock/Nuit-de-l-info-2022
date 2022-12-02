bd9cbbf05de5e1557f72c85d8d2b47dd20809f97
MONTMAIN JONATHAN p2100659 <jonathan.montmain@etu.univ-lyon1.fr>
2022-12-02 01:38:28 +0100

    Ajout d'affichage d'images anglées

--- assets/js/script.js
+++ assets/js/script.js
@@ -553,7 +553,9 @@ const drawBoard = () => {
 
     for (let i = 0; i < settings.snake.length; i++) {
         if (i == 0) {
+            context.rotate(Math.PI);
             context.drawImage(head, settings.snake[0][0] * unit, settings.snake[0][1] * unit, unit, unit);
+            context.restore();
         }
     }
 };
